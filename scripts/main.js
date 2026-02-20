84dae22158165ba737a98163d9acc3b50e1d62db
svchost <m.mattiabaraldi@gmail.com>
2026-02-20 23:28:25 +0100

    b

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,8 @@
-import {MaterialBrowser} from "./apps/materialBrowser.js";
-import {AssetBrowser} from "./apps/assetBrowser.js";
-import {TokenBrowser, setHudHook} from "./apps/tokenBrowser.js";
-import {QuickTerrain} from "./apps/quickTerrain.js";
-import {EffectBrowser} from "./apps/effectBrowser.js";
-import {BuildPanel} from "./apps/buildPanel.js";
-import {QuickEnvironment} from "./apps/quickEnvironment.js";
-import {CutscenePanel} from "./apps/cutscenePanel.js";
-import {RoomBuilder} from "./apps/roomBuilder.js";
-import { HeightmapPainter } from "./apps/heightmapPainter.js";
-
-BuildPanel.setHook();
-
-setHudHook();
 
 Hooks.on("3DCanvasConfig", (config) => {
-  const UI = config.UI;
-  UI.MaterialBrowser = MaterialBrowser;
-  UI.AssetBrowser = AssetBrowser;
-  UI.TokenBrowser = TokenBrowser;
-  UI.QuickTerrain = QuickTerrain;
-  UI.EffectBrowser = EffectBrowser;
-  UI.BuildPanel = BuildPanel;
-  UI.QuickEnvironment = QuickEnvironment;
-  UI.CutscenePanel = CutscenePanel;
-  UI.RoomBuilder = RoomBuilder;
-  Hooks.callAll("3DCanvasMapmakingPackRegisterAssetPacks", UI.AssetBrowser);
-  Hooks.callAll("3DCanvasMapmakingPackRegisterTokenPacks", UI.TokenBrowser);
+    const UI = config.UI;
+    Hooks.callAll("3DCanvasMapmakingPackRegisterAssetPacks", UI.AssetBrowser);
+    Hooks.callAll("3DCanvasMapmakingPackRegisterTokenPacks", UI.TokenBrowser);
 })
 
 Hooks.on("3DCanvasMapmakingPackRegisterAssetPacks", (ab) => {
@@ -52,11 +29,11 @@ Hooks.on("3DCanvasMapmakingPackRegisterAssetPacks", (ab) => {
             {
                 name: "Nature",
                 query: "nature",
-          },
-          {
-            name: "Rock Formations",
-            query: "Rocks_highend,Rocks_desert,Rocks_runic",
-          },
+            },
+            {
+                name: "Rock Formations",
+                query: "Rocks_highend,Rocks_desert,Rocks_runic",
+            },
             {
                 name: "Sci-Fi",
                 query: "scifi,cyberpunk,space",
@@ -99,135 +76,3 @@ Hooks.on("3DCanvasMapmakingPackRegisterAssetPacks", (ab) => {
         },
     );
 });
-
-
-Hooks.on("renderTileConfig", injectMaterialBrowser)
-Hooks.on("renderTokenConfig", injectMaterialBrowser);
-Hooks.on("renderPrototypeTokenConfig", injectMaterialBrowser);
-Hooks.on("renderShaderConfig", injectMaterialBrowser);
-Hooks.on("renderRoomBuilder", injectMaterialBrowser);
-
-Hooks.on("renderTokenConfig", async (app, html) => {
-    if (!game.modules.get("levels-3d-preview")?.active) return;
-    html = $(html);
-    function wait(ms) {
-        return new Promise((resolve) => setTimeout(resolve, ms));
-    }
-        await wait(100);
-
-    while (!html.find(`[name="flags.levels-3d-preview.model3d"]`).length) {
-        await wait(100);
-  }
-            const filepicker = html.find(`[name="flags.levels-3d-preview.model3d"]`).closest(".form-group");
-            TokenBrowser.create(filepicker, app);
-});
-
-Hooks.on("renderPrototypeTokenConfig", async (app, html) => {
-    if (!game.modules.get("levels-3d-preview")?.active) return;
-    function wait(ms) {
-        return new Promise((resolve) => setTimeout(resolve, ms));
-    }
-        await wait(100);
-
-    while (!html.querySelector(`[name="flags.levels-3d-preview.model3d"]`)) {
-        await wait(100);
-  }
-            const filepicker = html.querySelector(`[name="flags.levels-3d-preview.model3d"]`).closest(".form-group");
-            TokenBrowser.create(filepicker, app);
-});
-
-async function injectMaterialBrowser(app, html) {
-  if(!game.modules.get("levels-3d-preview")?.active) return;
-  html = $(html);
-  function wait(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
-
-  if (app.id == "levels-3d-preview-shader-config") {
-    const filepickers = html.find(`file-picker[type="image"]`)
-    filepickers.each((i, el) => { 
-      if (el.name == "mask.textureMask" || el.name == "splatMap.textureSplatMap") {
-        HeightmapPainter.create(el)
-      } else {
-        MaterialBrowser.create($(el).closest(".form-group"), app, "_Color");
-      }
-    });
-    
-  } else if (app.id == "room-builder") {
-    const filepickers = html.find(`input.image`)
-    filepickers.each((i, el) => { 
-      MaterialBrowser.create($(el).closest(".form-group"), app);
-    });
-  } else {
-        await wait(100);
-
-          while (!html.find(`[name="flags.levels-3d-preview.imageTexture"]`).length) {
-              await wait(100);
-          }
-
-          const filepicker = html.find(`[name="flags.levels-3d-preview.imageTexture"]`).closest(".form-group");
-          MaterialBrowser.create(filepicker, app);
-  }
-
-}
-
-Hooks.on("renderMapGen", (app, html) => {
-  html.find('input.image').closest('.form-group').each((i, el) => {
-    MaterialBrowser.create($(el), app);
-  });
-});
-
-Hooks.on("renderWallConfig", async (app,html) => {
-  if(!game.modules.get("levels-3d-preview")?.active) return;
-  function wait(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
-
-  while(!html.find(`input[name="flags.levels-3d-preview.wallTexture"]`).length) {
-    await wait(100);
-  }
-
-  const filepicker = html.find(`input[name="flags.levels-3d-preview.wallTexture"]`).closest(".form-group");
-  MaterialBrowser.create(filepicker, app);
-  const filepicker2 = html.find(`input[name="flags.levels-3d-preview.wallSidesTexture"]`).closest(".form-group");
-  MaterialBrowser.create(filepicker2, app);
-})
-
-Hooks.once("ready", () => {
-  // Module title
-  const MODULE_ID = "canvas3dcompendium";
-  const MODULE_TITLE = "3D Canvas Mapmaking Pack";
-
-  const FALLBACK_MESSAGE_TITLE = MODULE_TITLE;
-  const FALLBACK_MESSAGE = `
-  <h2>3D Canvas Mapmaking Pack License Agreement</h3>
-  <p>By using this module, you understand that some of the included assets can be used <strong>only inside 3D Canvas</strong>.</p>
-  <p>By clicking <strong>'I Accept'</strong> you confirm that you read the asset specific licensing linked below.</p>
-  <p>Check <a href="https://github.com/theripper93/canvas3dcompendium">HERE</a> for the specific license of every included asset.</p>`;
-
-  // Settings key used for the "Don't remind me again" setting
-  const DONT_REMIND_AGAIN_KEY = "canvas3dcompendiumLicenseAgreement";
-
-  // Dialog code
-  game.settings.register(MODULE_ID, DONT_REMIND_AGAIN_KEY, {
-    name: "",
-    default: false,
-    type: Boolean,
-    scope: "world",
-    config: false,
-  });
-  if (game.user.isGM && !game.settings.get(MODULE_ID, DONT_REMIND_AGAIN_KEY)) {
-    new Dialog({
-      title: FALLBACK_MESSAGE_TITLE,
-      content: FALLBACK_MESSAGE,
-      buttons: {
-        ok: { icon: '<i class="fas fa-check"></i>', label: "I Accept", callback: () => game.settings.set(MODULE_ID, DONT_REMIND_AGAIN_KEY, true), },
-        dont_remind: {
-          icon: '<i class="fas fa-times"></i>',
-          label: "I Refuse",
-        },
-      },
-    }).render(true);
-  }
-});
-
